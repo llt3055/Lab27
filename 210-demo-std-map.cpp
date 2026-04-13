3191029e2589a52d4b1e1bcdcc192a5567ba9270
llt3055 <llt30555@gmail.com>
2026-04-13 10:40:27 +0800

    try to complete milestone3

--- 210-demo-std-map.cpp
+++ 210-demo-std-map.cpp
@@ -29,7 +29,16 @@ int main() {
     villagers["Kyle"] = make_tuple(10, "Wolf", "Hubba hubba!");
     villagers["Raymond"] = make_tuple(8, "Cat", "Nice fit");
 
-    display_villagers(villagers, "Milestone 2 - Current Villager Map:");
+    int choice = 0;
+    while (choice = 4) {
+        cout << "\n1. Increase Friendship" << endl;
+        cout << "2. Decrease Friendship" << endl;
+        cout << "3. Search for Villager" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+    if (choice == 4) break;
 
     string searchKey = "Drago";
     auto it = villagers.find(searchKey);
